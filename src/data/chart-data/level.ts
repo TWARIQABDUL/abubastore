e19b5a6457135af2c02a3c3171401233c788f020
TWARIQABDUL <tariqazizibabu@gmail.com>
2024-11-20 10:26:06 +0200

    dashbord first commit

---
+++ src/data/chart-data/level.ts
@@ -0,0 +1,9 @@
+interface LevelProps {
+  Volume: number[];
+  Service: number[];
+}
+
+export const levelData: LevelProps = {
+  Volume: [32, 80, 65, 43, 82, 100],
+  Service: [78, 77, 92, 114, 75, 52],
+};
